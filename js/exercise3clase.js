260e5b32da45c5f101ffc088853c15509fbefb1d
Julieta Correa <mjcorrea210@gmail.com>
2023-04-03 23:28:45 -0300

    cambios en el ejercicio 3

--- js/exercise3clase.js
+++ js/exercise3clase.js
@@ -25,20 +25,18 @@ class Rectangulo {
       return this.alto * this.ancho;
     }
   }
-// Crear un objeto "rectángulo" con alto de 5 y ancho de 10
+
 const rectangulo1 = new Rectangulo(5, 10);
 
-// Mostrar las propiedades del rectángulo
-rectangulo1.mostrarPropiedades(); // Output: Alto: 5, Ancho: 10
 
-// Modificar el alto del rectángulo a 7
+rectangulo1.mostrarPropiedades(); 
+
 rectangulo1.modificarAlto(7);
 
-// Calcular y mostrar el perímetro del rectángulo
-const perimetro = rectangulo1.calcularPerimetro();
-console.log(`Perímetro: ${perimetro}`); // Output: Perímetro: 34
 
-// Calcular y mostrar el área del rectángulo
+const perimetro = rectangulo1.calcularPerimetro();
+console.log(`Perímetro: ${perimetro}`); 
+
 const area = rectangulo1.calcularArea();
-console.log(`Área: ${area}`); // Output: Área: 70
+console.log(`Área: ${area}`); 
   
